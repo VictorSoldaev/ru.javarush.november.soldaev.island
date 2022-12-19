c4512fa3f407d02b984ea64e4d9434a471dfc0ab
Victor Soldaev <gresherpw@yandex.ru>
2022-12-20 01:11:16 +0300

    Добавлен конструктор базовых характеристик

--- soldaev.island/unit/plant/Plant.java
+++ soldaev.island/unit/plant/Plant.java
@@ -1,6 +1,12 @@
 package unit.plant;
 
+import setting.BaseStatsUnit;
 import unit.Organizm;
 
 public abstract class Plant extends Organizm {
+    protected final BaseStatsUnit baseStatsUnit;
+
+    protected Plant(BaseStatsUnit baseStatsUnit) {
+        this.baseStatsUnit = baseStatsUnit;
+    }
 }

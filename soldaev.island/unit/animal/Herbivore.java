2b30dfbc516aa73f7bca489d0f722e27ed17ef47
Victor Soldaev <gresherpw@yandex.ru>
2022-12-20 01:10:13 +0300

    Интерфейс травоялные проработа теперь травоядные могут шипать трафку

--- soldaev.island/unit/animal/Herbivore.java
+++ soldaev.island/unit/animal/Herbivore.java
@@ -1,15 +1,52 @@
 package unit.animal;
 
+import location.Earth;
+import servis.CoordinateHandler;
 import unit.Organizm;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
-public interface Herbivore {
-    default boolean lookingForGrass(ArrayList<Organizm> stepFood){
+public interface Herbivore extends CoordinateHandler {
+    default float lookingForGrass(int x, int y, Earth earth) {
+        int counter = 8;
+        int speed = ((Animal) this).baseStatsUnit.speed;
+        for (int i = 0; i < counter; i++) {
+            int xG = getCoordinateX(x, earth, speed);
+            int yG = getCoordinateY(y, earth, speed);
+            if (checkGress(earth.getArrayListAnimals(xG, yG))) {
+                return eatGrass(earth.getArrayListAnimals(xG, yG));
+            }
+        }
+        return 0;
+    }
 
-        // получаем список травы
-        // если она есть кушаем
-        // пере определяем метод для утки и ее подобных если нет травы ищем гусеницу
-        return true;
+    default boolean checkGress(ArrayList<Organizm> stepFood) {
+        if (stepFood.get(0) == null) {
+            return false;
+        } else {
+            if (stepFood.get(0).getClass().getSimpleName().equals("Grass")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    default float eatGrass(ArrayList<Organizm> stepFood) {
+        float satietyPositive = 0;
+        for (Organizm o :
+                stepFood) {
+            try {
+                Field field = o.getClass().getDeclaredField("hp");
+                field.setAccessible(true);
+                satietyPositive = satietyPositive + (float) field.get(o);
+                stepFood.remove(o);
+            } catch (Exception ignor) {
+                System.out.println("Что-то пошло не так");
+            }
+            return satietyPositive;
+
+        }
+        return 0;
     }
 }

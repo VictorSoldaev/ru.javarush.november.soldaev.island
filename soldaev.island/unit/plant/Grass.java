87050a6946ebb0ac6cb4d31e122c4a337d6b0cc6
Victor Soldaev <gresherpw@yandex.ru>
2022-12-20 01:08:54 +0300

    Добавлен конструктор с базовыми характеристиками

--- soldaev.island/unit/plant/Grass.java
+++ soldaev.island/unit/plant/Grass.java
@@ -1,4 +1,11 @@
 package unit.plant;
 
+import setting.BaseStatsUnit;
+
 public class Grass extends Plant{
+    private float hp;
+    public Grass() {
+        super(BaseStatsUnit.STATS_BASE_GRASS);
+        this.hp = BaseStatsUnit.STATS_BASE_CATERPILLAR.weight;
+    }
 }

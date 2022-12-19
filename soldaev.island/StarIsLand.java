5d490d8c16a6610e37d897fcbd07131d81d326f5
Victor Soldaev <gresherpw@yandex.ru>
2022-12-20 01:13:38 +0300

    Убрано закомиченое запускает тест создания мира

--- soldaev.island/StarIsLand.java
+++ soldaev.island/StarIsLand.java
@@ -1,50 +1,10 @@
-import setting.Setting;
-import unit.Organizm;
-import unit.animal.*;
-
-import java.util.ArrayList;
+import servis.World;
 
 public class StarIsLand {
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
-//        World world = new World();
-//        world.createLife();
-//        Setting setting = new Setting();
-//        Bear bear = new Bear();
-//        bear.setSatiety(bear.getSatiety() - 10);
-//        System.out.println(bear.getSatiety());
-//        Mouse mouse = new Mouse();
-//        Horse horse = new Horse();
-//        System.out.println("Начинаем охоту");
-//        for (int i = 0; i < 50; i++) {
-//            bear.toEat(horse);
-//        }
-//        System.out.println(bear.getSatiety());
-//        UnitFactory factory = new UnitFactory();
-//        ArrayList<Organizm> lock = new ArrayList<>();
-//        Earth sdfsdf = new Earth(120, 20);
-//
-//        if (true) {
-//            sdfsdf.add(factory.create(bear.multiply()), 5);
-//
-//        }
-
-//        Organizm org = new Bear();
-//        Class<?>[] interfaces = org.getClass().getInterfaces();
-//        for (Class c :
-//                interfaces) {
-//            String w = c.getSimpleName();
-//            System.out.println(w);
-//            if(w.equals("Predator")){
-//                System.out.println("Tru");
-//            }
-//
-//        }
-
-        Setting setting = new Setting();
-        Bear bear = new Bear();
-        ArrayList<Organizm> lock = new ArrayList<>();
-
-
+    public static void main(String[] args)  {
+        System.out.println("Начинаем создание мира!");
+        World world = new World();
+        System.out.println("Мир создан!");
 
     }
 }

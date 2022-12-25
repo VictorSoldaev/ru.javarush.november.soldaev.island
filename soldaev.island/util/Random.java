05ff5f73c6f8f044f8558446efd2c7fffab60b92
Victor Soldaev <gresherpw@yandex.ru>
2022-12-26 00:41:43 +0300

    Смена папки

---
+++ soldaev.island/util/Random.java
@@ -0,0 +1,9 @@
+package util;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public interface Random {
+    default int random(int t, int n) {
+        return ThreadLocalRandom.current().nextInt(t, n);
+    }
+}
